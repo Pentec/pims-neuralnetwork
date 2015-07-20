3bd253159a1f5d3e4bf854b1f8daef922c54c4b5
Trevor Austin <taustin.swim@gmail.com>
2015-07-20 15:22:39 +0200

    Added some basic AI testing

--- neuralnetwork.js
+++ neuralnetwork.js
@@ -1,1 +1,12 @@
+var test = require('./mockFiles/test.json');
+var synaptic = require('synaptic');
+var Architect = synaptic.Architect;
 
+var perceptron = new Architect.Perceptron(2,3,1);
+
+perceptron.trainer.train(test);
+
+console.log(perceptron.activate([1,0]));
+console.log(perceptron.activate([0,0]));
+console.log(perceptron.activate([0,1]));
+console.log(perceptron.activate([1,1]));

8bfa631044f6e4d59e564b28b408e5b4065e3116
Trevor Austin <taustin.swim@gmail.com>
2015-07-21 14:10:53 +0200

    Creating a node-module

--- neuralnetwork.js
+++ neuralnetwork.js
@@ -1,12 +1,26 @@
-var test = require('./mockFiles/test.json');
 var synaptic = require('synaptic');
 var Architect = synaptic.Architect;
 
 var perceptron = new Architect.Perceptron(2,3,1);
 
-perceptron.trainer.train(test);
+/** Function activates using the form inputs and determines the length of admission for the patient
+ *  @param input
+ * */
+exports.activate = function(input){
 
-console.log(perceptron.activate([1,0]));
-console.log(perceptron.activate([0,0]));
-console.log(perceptron.activate([0,1]));
-console.log(perceptron.activate([1,1]));
+}
+
+/**
+ *
+ */
+exports.train = function(){
+
+}
+
+/**
+ * Adds a trainer to the training data
+ * @param input
+ */
+function addTrainer(input){
+
+}
